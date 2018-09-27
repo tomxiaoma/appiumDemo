50367baff93c2598288fa9e89f6c09113528d49f
mawentao <545736616@qq,com>
2018-09-27 17:57:34 +0800

    修改执行方式

--- src/test/java/AppiumDemoTest.java
+++ src/test/java/AppiumDemoTest.java
@@ -1,23 +1,21 @@
-
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.touch.offset.PointOption;
-import org.openqa.selenium.By;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+public class AppiumDemoTest {
 
-public class AppiumDemo {
+    private AppiumDriver driver;
 
-    private  static AppiumDriver driver;
-
-    public static void main(String[] args) throws MalformedURLException, InterruptedException {
-
+    @BeforeTest
+    public void initApium() throws MalformedURLException, InterruptedException {
         File classpathRoot = new File(System.getProperty("user.dir"));
         File appDir = new File(classpathRoot,"app");
         File app = new File(appDir,"yiqizhangda.apk");
@@ -35,17 +33,13 @@ public class AppiumDemo {
         capabilities.setCapability("appPackage", "com.yiqizhangda.parent.test");
         capabilities.setCapability("appActivity","com.kid17.parent.ui.splash.SplashActivity" );
         capabilities.setCapability("appWaitActivity","com.kid17.parent.ui.splash.SplashActivity" );
-        AppiumDriver driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-
+        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         Thread.sleep(5000);
 
-       /* //new TouchAction(driver).tap(415, 621).perform();
-        new TouchAction(driver).press(PointOption.point(765, 573)).moveTo(PointOption.point(3, 613)).release().perform();
-        Thread.sleep(1000);
-        new TouchAction(driver).press(PointOption.point(765, 573)).moveTo(PointOption.point(8, 573)).release().perform();
-        Thread.sleep(1000);
-        new TouchAction(driver).press(PointOption.point(765, 573)).moveTo(PointOption.point(8, 562)).release().perform();
-        Thread.sleep(1000);*/
+    }
+
+    @Test
+    public void TestAppiumDemo() throws InterruptedException {
 
         for (int i=0;i<5;i++){
             SwipeScreen.swipeLeft(driver);
@@ -59,16 +53,15 @@ public class AppiumDemo {
         Thread.sleep(3000);
         driver.findElementById("com.yiqizhangda.parent.test:id/et_pwd_activity_input_pwd").sendKeys("123123");
         driver.findElementById("com.yiqizhangda.parent.test:id/btn_next_step_activity_input_pwd").click();
+    }
 
-
-
-
-
+    @AfterTest
+    public void closeApp() throws InterruptedException {
         Thread.sleep(3000);
-
-
         driver.quit();
 
     }
 
 }
+
+
